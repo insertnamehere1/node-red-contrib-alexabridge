fe21b47108aecb41a2760bcaff11e0674b8716a5
insertnamehere2 <chris@chrisandbev.com.au>
2017-01-22 20:53:50 +1100

    Added code to allow to run as node in Node-Red

---
+++ config.js
@@ -0,0 +1,118 @@
+
+    var bridgeXml = ['<?xml version="1.0" encoding="UTF-8"?>',
+        '<root xmlns="urn:schemas-upnp-org:device-1-0">',
+        '<specVersion>',
+        '<major>1</major>',
+        '<minor>0</minor>',
+        '</specVersion>',
+        '<URLBase>http://192.168.1.128:8082/</URLBase>',
+        '<device>',
+        '<deviceType>urn:schemas-upnp-org:device:Basic:1</deviceType>',
+        '<friendlyName>Node-Red-Hue-Bridge (192.168.42.128)</friendlyName>',
+        '<manufacturer>Royal Philips Electronics</manufacturer>',
+        '<manufacturerURL>http://www.philips.com</manufacturerURL>',
+        '<modelDescription>Hue Emulator for Node Red</modelDescription>',
+        '<modelName>Philips hue bridge 2015</modelName>',
+        '<modelNumber>BSB002</modelNumber>',
+        '<modelURL>http://www.meethue.com</modelURL>',
+        '<serialNumber>12345</serialNumber>',
+        '<UDN>uuid:88f6698f-2c83-4393-bd03-cd54a9f8595</UDN>',
+        '<serviceList>',
+        '<service>',
+        '<serviceType>(null)</serviceType>',
+        '<serviceId>(null)</serviceId>',
+        '<controlURL>(null)</controlURL>',
+        '<eventSubURL>(null)</eventSubURL>',
+        '<SCPDURL>(null)</SCPDURL>',
+        '</service>',
+        '</serviceList>',
+        '<presentationURL>index.html</presentationURL>',
+        '<iconList>',
+        '<icon>',
+        '<mimetype>image/png</mimetype>',
+        '<height>48</height>',
+        '<width>48</width>',
+        '<depth>24</depth>',
+        '<url>hue_logo_0.png</url>',
+        '</icon>',
+        '<icon>',
+        '<mimetype>image/png</mimetype>',
+        '<height>120</height>',
+        '<width>120</width>',
+        '<depth>24</depth>',
+        '<url>hue_logo_3.png</url>',
+        '</icon>',
+        '</iconList>',
+        '</device>',
+        '</root>'].join("\n");
+
+    var lightsJson = {
+        "lights": { "5102d46c-50d5-4bc7-a180-38623e4bbb08":
+        {
+            "state":
+            {
+                "on": true,
+                "bri": 254,
+                "hue": 15823,
+                "sat": 88,
+                "effect": "none",
+                "ct": 313,
+                "alert": "none",
+                "colormode": "ct",
+                "reachable": true,
+                "xy": [0.4255, 0.3998]
+            },
+            "type": "Extended color light",
+            "name": "test 1 light",
+            "modelid": "LCT001",
+            "manufacturername": "Philips",
+            "uniqueid": "5102d46c-50d5-4bc7-a180-38623e4bbb08",
+            "swversion": "65003148",
+            "pointsymbol":
+            {
+                "1": "none",
+                "2": "none",
+                "3": "none",
+                "4": "none",
+                "5": "none",
+                "6": "none",
+                "7": "none",
+                "8": "none"
+            }
+        },
+        "69d4f390-9bef-468b-b58e-4495027ff33c":
+        {
+            "state":
+            {"on": false,
+                "bri": 254,
+                "hue": 15823,
+                "sat": 88,
+                "effect": "none",
+                "ct": 313,
+                "alert": "none",
+                "colormode": "ct",
+                "reachable": true,
+                "xy": [0.4255, 0.3998]
+            },
+            "type": "Extended color light",
+            "name": "test 2 light",
+            "modelid": "LCT001",
+            "manufacturername": "Philips",
+            "uniqueid": "69d4f390-9bef-468b-b58e-4495027ff33c",
+            "swversion": "65003148",
+            "pointsymbol":
+            {"1": "none",
+                "2": "none",
+                "3": "none",
+                "4": "none",
+                "5": "none",
+                "6": "none",
+                "7": "none",
+                "8": "none"}
+        }
+    }
+    };
+
+
+exports.bridgeXml = bridgeXml;
+exports.lightsJson = lightsJson;
--- hue-bridge.js
+++ hue-bridge.js
@@ -1,93 +1,109 @@
-var fs = require('fs');
+var configuration = require('./config');
+
 var http = require('http');
 var ssdp = require("peer-ssdp");
 var ssdpPeer = ssdp.createPeer();
 
+module.exports = function(RED) {
+
+    function HueBridge(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+
 // handle ssdpPeer ready event.
-ssdpPeer.on("ready",function(){
-    console.log("UPNP server listening on port 1900.");
-});
+        ssdpPeer.on("ready", function () {
+            console.log("UPNP server listening on port 1900.");
+        });
 
 // handle ssdpPeer close event. This event will be emitted after `ssdpPeer.close()` is called.
-ssdpPeer.on("close",function(){
-    console.log("UPNP server Closing.");
-});
+        ssdpPeer.on("close", function () {
+            console.log("UPNP server Closing.");
+        });
 
 // handle SSDP M-SEARCH messages.
 // param headers is JSON object containing the headers of the SSDP M-SEARCH message as key-value-pair.
 // param address is the socket address of the sender
-ssdpPeer.on("search",function(headers, address){
+        ssdpPeer.on("search", function (headers, address) {
 
-    console.log("SEARCH:",headers,address);
+            console.log("SEARCH:", headers, address);
 
-    if (headers.ST && headers.MAN=='"ssdp:discover"') {
-        ssdpPeer.reply({
-            NT: "urn:schemas-upnp-org:device:basic:1",
-            SERVER: "node.js/0.10.28 UPnP/1.1",
-            ST: "urn:schemas-upnp-org:device:basic:1",
-            USN: "uuid:Socket-1_0-221438K0100073::urn:Belkin:device:**",
-            LOCATION: "http://{{networkInterfaceAddress}}:8082/description.xml"
-        }, address);
-    }
-});
+            if (headers.ST && headers.MAN == '"ssdp:discover"') {
+                ssdpPeer.reply({
+                    NT: "urn:schemas-upnp-org:device:basic:1",
+                    SERVER: "node.js/0.10.28 UPnP/1.1",
+                    ST: "urn:schemas-upnp-org:device:basic:1",
+                    USN: "uuid:Socket-1_0-221438K0100073::urn:Belkin:device:**",
+                    LOCATION: "http://{{networkInterfaceAddress}}:8082/description.xml"
+                }, address);
+            }
+        });
 
-// Start ssdpPeer, when ready the `ready` event will be emitted.
-ssdpPeer.start();
+        // Start ssdpPeer, when ready the `ready` event will be emitted.
+        ssdpPeer.start();
 
-// load the file containing connected devices list
-var connectedDevices = JSON.parse(fs.readFileSync('lights.json'));
+        // load the file containing connected devices list
+        var connectedDevices = configuration.lightsJson;
 
-const PORT=8082;
+        const PORT = 8082;
 
-function handleRequest(request, response){
+        function handleRequest(request, response) {
 
-    console.log(request.method, request.url);
+            console.log(request.method, request.url);
 
-    var lightMatch = /^\/api\/(\w*)\/lights\/([\w\-]*)/.exec(request.url);
+            var lightMatch = /^\/api\/(\w*)\/lights\/([\w\-]*)/.exec(request.url);
 
-    if (lightMatch) {
+            if (lightMatch) {
 
-        // request to turn light on or off
-        if (request.method == 'PUT') {
-            request.on('data', function(chunk) {
-                console.log("Received PUT data:",chunk.toString());
-                request.data = JSON.parse(chunk);
-            });
-            request.on('end', function() {
-                response.writeHead(200, "OK", {'Content-Type': 'application/json'});
-                var responseStr='[{"success":{"/lights/'+lightMatch[2]+'/state/on":'+request.data.on+'}}]';
-                console.log("Sending response:",responseStr);
-                response.end(responseStr);
-            });
+                // request to turn light on or off
+                if (request.method == 'PUT') {
+                    request.on('data', function (chunk) {
+                        console.log("Received PUT data:", chunk.toString());
+                        request.data = JSON.parse(chunk);
+                    });
+                    request.on('end', function () {
+                        response.writeHead(200, "OK", {'Content-Type': 'application/json'});
+                        var responseStr = '[{"success":{"/lights/' + lightMatch[2] + '/state/on":' + request.data.on + '}}]';
+                        console.log("Sending response:", responseStr);
+                        response.end(responseStr);
 
-        // request for individual light state
-        } else {
-            console.log("Sending light ", lightMatch[2]);
-            response.writeHead(200, {'Content-Type': 'application/json'});
-            response.end(JSON.stringify(connectedDevices.lights[lightMatch[2]]));
+                        // debugging output
+                        var msg = {payload:responseStr};
+                        node.send(msg);
+                    });
+
+                    // request for individual light state
+                } else {
+                    console.log("Sending light ", lightMatch[2]);
+                    response.writeHead(200, {'Content-Type': 'application/json'});
+                    response.end(JSON.stringify(connectedDevices.lights[lightMatch[2]]));
+                }
+
+            } else {
+
+                // api request for configuration of lights connected to the bridge
+                if (/^\/api/.exec(request.url)) {
+                    console.log("Sending light details");
+                    response.writeHead(200, {'Content-Type': 'application/json'});
+                    response.end(JSON.stringify(connectedDevices));
+
+                    // request for description.xml which is bridge configuration
+                } else if (request.url == '/description.xml') {
+                    var setup = configuration.bridgeXml;
+                    console.log("Sending bridgeXml");
+                    response.writeHead(200, {'Content-Type': 'application/xml'});
+                    response.end(setup);
+                }
+            }
         }
 
-    } else {
-
-        // api request for configuration of lights connected to the bridge
-        if (/^\/api/.exec(request.url)) {
-            console.log("Sending lights.json");
-            response.writeHead(200, {'Content-Type': 'application/json'});
-            response.end(JSON.stringify(connectedDevices));
-
-        // request for description.xml which is bridge configuration
-        } else if (request.url == '/description.xml') {
-            var setup = fs.readFileSync('bridgeConfig.xml');
-            console.log("Sending bridgeConfig.xml");
-            response.writeHead(200, {'Content-Type': 'application/xml'});
-            response.end(setup);
-        }
-    }
-}
-
-var server = http.createServer(handleRequest);
+        var server = http.createServer(handleRequest);
 
 // Start the local server listening for connections
-server.listen(PORT, function(){
-    console.log("Hue Bridge listening on: http://localhost:%s", PORT);
-});
+        server.listen(PORT, function () {
+            console.log("Hue Bridge listening on: http://localhost:%s", PORT);
+        });
+    }
+
+    RED.nodes.registerType("Hue Bridge",HueBridge);
+};
